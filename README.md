1fc874b08d5352d51518c556ebb064926fa9cde9
bulhwi <mm225012@gmail.com>
2021-09-06 22:14:56 +0900

    backend 구성, npm i axios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 * [NUXT Installation](https://nuxtjs.org/docs/2.x/get-started/installation)
+* [JSON Placeholder](https://jsonplaceholder.typicode.com/) - 목 api 제공하는 fake server
+* [json-server](https://github.com/typicode/json-server)
--- learn-nuxt/pages/main.vue
+++ learn-nuxt/pages/main.vue
@@ -7,8 +7,14 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'Main',
+  async created() {
+    const response = await axios.get("http://localhost:3000/products");
+    console.log(response);
+  }
 };
 </script>
 
